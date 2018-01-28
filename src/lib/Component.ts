7dd318b06c45d503d805cac9ffc6bccfcb816a64
ko-tori <alex.shi0915@gmail.com>
2018-01-28 03:48:29 -0600

    ?

--- src/lib/Component.ts
+++ src/lib/Component.ts
@@ -14,8 +14,10 @@ interface Link {
 
 export class Component {
 	private components: Set<GUID>;
-	private locationMap: Map<GUID, Vector<number>>;
+	private location: Vector<number>;
 	private links: Set<Link>;
+	private absoluteInputs: Set<IO>;
+	private absoluteOutputs: Set<IO>;
 
 	private evaluate: ((inputs: BitArray) => BitArray);
 
@@ -28,10 +30,21 @@ export class Component {
 	static from(inner: Component): Component {
 		let outer = new Component();
 		outer.components.add(inner.guid);
+		for(let io of inner.absoluteOutputs) {
+			outer.absoluteOutputs.add(io);
+		}
+		for(let io of inner.absoluteInputs) {
+			outer.absoluteInputs.add(io);
+		}
 		return outer;
 	}
+
 	constructor(private inputs: Link[] = [], private outputs: Component[] = []) {
 		this.guid = GUID.new();
+
+	}
+
+	compose(): void {
 
 	}
 
@@ -39,17 +52,29 @@ export class Component {
 		if (this.components.has(link.from.guid)) {
 			this.components.add(link.to.guid);
 			this.links.add(link);
+			this.absoluteOutputs.remove(link.from);
+			for(let io of link.to.guid.absoluteOutputs) {
+				this.absoluteOutputs.add(io);
+			}
 			return true;
 		}
-		if (this.components.has(link.from.guid)) {
-			this.components.add(link.to.guid);
+		if (this.components.has(link.to.guid)) {
+			this.components.add(link.from.guid);
 			this.links.add(link);
+			this.absoluteInputs.remove(link.to);
+			for(let io of link.from.guid.absoluteInputs) {
+				this.absoluteInputs.add(io);
+			}
 			return true;
 		}
 		return false;
 	}
 }
 
-export class ANDComponent {
-
+export class ANDComponent extends Component {
+	private evaluate(p: BitArray): BitArray {
+		let r = new BitArray();
+		r.set(0, p[0] & p[1]);
+		return r;
+	}
 }
